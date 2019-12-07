aa32bc21f9396fe5cbe5ff2b8db9cdcf24a7438a
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-07 17:13:15 +0530

    Added:Calculated Operation 2

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -6,5 +6,9 @@ read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter Third Number : " c
 
-result=$((a+(b*c)))
+result=$(($a+($b*$c)))
+result1=$((($a*$b)+$c))
+
 echo "$result"
+
+echo "$result1"

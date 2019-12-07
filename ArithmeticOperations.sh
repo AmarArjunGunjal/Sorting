4ee5b8941e83bc888afbdfe6da0c1b4e3aa26672
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-07 17:23:14 +0530

    Calculating Operation 4

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -9,9 +9,12 @@ read -p "Enter Third Number : " c
 result=$(($a+($b*$c)))
 result1=$((($a*$b)+$c))
 result2=$(($c+($a/$b)))
+result3=$((($a%$b)+c))
 
 echo "$result"
 
 echo "$result1"
 
 echo "$result2"
+
+echo "$result3"

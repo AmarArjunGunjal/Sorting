edb431ae45b58644fac4d2e0a491dbb1d170a016
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-07 17:54:21 +0530

    Added : Storing Dictionary Value in array

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -2,27 +2,33 @@
 
 echo "Welcome"
 
-declare -A dictResult
+declare -A resultDict
 read -p "Enter First Number : " a
 read -p "Enter Second Number : " b
 read -p "Enter Third Number : " c
 
-result=$(($a+($b*$c)))
-result1=$((($a*$b)+$c))
-result2=$(($c+($a/$b)))
-result3=$((($a%$b)+c))
+result1=$(($a+($b*$c)))
+result2=$((($a*$b)+$c))
+result3=$(($c+($a/$b)))
+result4=$((($a%$b)+c))
 
-echo "$result"
+#echo "$result"
 
-echo "$result1"
+#echo "$result1"
 
-echo "$result2"
+#echo "$result2"
 
-echo "$result3"
+#echo "$result3"
 
 
-resultDict[1]=$result
-resultDict[2]=$result1
-resultDict[3]=$result2
-resultDict[4]=$result3
+resultDict["result 1"]=$result1
+resultDict["result 2"]=$result2
+resultDict["result 3"]=$result3
+resultDict["result 4"]=$result4
 
+for (( i=1; i<=${#resultDict[@]}; i++ ))
+do
+	arrayResult[$i]="${resultDict["result $i"]}"
+done
+
+echo ${arrayResult[@]}
